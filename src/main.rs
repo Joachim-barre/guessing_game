8d52a03d063c99884b3411e063777b78004d72c3
Joachim-barre <joachim.barre@laposte.net>
2024-01-02 12:14:11 +0100

    moved printing messages to another function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,22 @@ use rand::Rng;
 
 const DEBUG : bool = false;
 
+fn print_diff(diff : u32){
+    if diff == 0 {
+        println!("You won!");
+    }else if diff < 5 {
+        println!("You are really close!");
+    }else if diff < 10 {
+        println!("You are close!");
+    }else if diff < 20 {
+        println!("You are close, but a little far!");
+    }else if diff < 40 {
+        println!("You are a little bit too far!");
+    }else {
+        println!("You are really far!")
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -31,21 +47,10 @@ fn main() {
 
         println!("You guessed: {guess}");
 
-        let diff = secret_number.abs_diff(guess);
-       
-        if diff == 0 {
-            println!("You won!");
+        print_diff(guess.abs_diff(secret_number));
+    
+        if guess == secret_number {
             break;
-        }else if diff < 5 {
-            println!("You are really close!");
-        }else if diff < 10 {
-            println!("You are close!");
-        }else if diff < 20 {
-            println!("You are close, but a little far!");
-        }else if diff < 40 {
-            println!("You are a little bit too far!");
-        }else {
-            println!("You are really far!")
         }
     }
 }
